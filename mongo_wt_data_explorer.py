6afeee0d896d3842bd41918e3dbbbcf72cfcc06b
Gregory Noma <gregory.noma@gmail.com>
2022-07-22 12:11:07 -0400

    timestamps

--- mongo_wt_data_explorer.py
+++ mongo_wt_data_explorer.py
@@ -1,19 +1,42 @@
+import re
 import subprocess
 import binascii
 import bson
 import sys
 import pprint
 
+
 wt_path = ""
 ksdecode_path = ""
 data_path = ""
+timestamp = None
 
-if not wt_path:
-    wt_path = input("Path to wt: ")
-if not ksdecode_path:
-    ksdecode_path = input("Path to ksdecode (optional): ")
-if not data_path:
-    data_path = input("Path to data files: ")
+
+def prompt_timestamp():
+    global timestamp
+
+    new_timestamp = input("Timestamp to read data at (leave empty for latest): ")
+
+    if not new_timestamp:
+        timestamp = None
+
+    if new_timestamp.isnumeric():
+        timestamp = int(new_timestamp)
+
+    found = re.compile("(\d+), ?(\d+)").findall(new_timestamp)
+    if found:
+        secs, inc = found[0]
+        timestamp = (int(secs) << 32) + int(inc)
+
+    print("Unable to interpret timestamp", new_timestamp)
+
+
+def timestamp_str():
+    return (
+        "Timestamp({}, {})".format(timestamp >> 32, timestamp % (1 << 32))
+        if timestamp
+        else ""
+    )
 
 
 def process_dump(
@@ -46,18 +69,25 @@ def process_dump(
 
 
 def dump(ident):
+    dump_cmd = [
+        wt_path,
+        "-r",
+        "-C",
+        "log=(compressor=snappy,path=journal)",
+        "-h",
+        data_path,
+        "dump",
+        "-x",
+    ]
+
+    if timestamp:
+        dump_cmd.append("-t")
+        dump_cmd.append(str(timestamp))
+
+    dump_cmd.append("table:" + ident)
+
     return subprocess.Popen(
-        [
-            wt_path,
-            "-r",
-            "-C",
-            "log=(compressor=snappy,path=journal)",
-            "-h",
-            data_path,
-            "dump",
-            "-x",
-            "table:" + ident,
-        ],
+        dump_cmd,
         stdout=subprocess.PIPE,
     )
 
@@ -118,12 +148,15 @@ def get_string_width(text):
 def explore_index(entry, index, position):
     collection_msg = "Collection " + entry["ns"]
     index_msg = "Index " + index
-    header_width = max(len(collection_msg), len(index_msg))
+    timestamp_msg = timestamp_str()
+    header_width = max(len(collection_msg), len(index_msg), len(timestamp_msg))
 
     while True:
         print("*" * header_width)
         print(collection_msg)
         print(index_msg)
+        if timestamp_msg:
+            print(timestamp_msg)
         print("*" * header_width)
         print("(b) back")
         print("(c) catalog entry")
@@ -188,15 +221,19 @@ def explore_index(entry, index, position):
 
 def explore_collection(entry):
     collection_msg = "Collection " + entry["ns"]
-    header_width = len(collection_msg)
+    timestamp_msg = timestamp_str()
+    header_width = max(len(collection_msg), len(timestamp_msg))
 
     indexes = []
-    for index in entry["idxIdent"]:
-        indexes.append(index)
+    if "idxIdent" in entry:
+        for index in entry["idxIdent"]:
+            indexes.append(index)
 
     while True:
         print("*" * header_width)
         print(collection_msg)
+        if timestamp_msg:
+            print(timestamp_msg)
         print("*" * header_width)
         print("(b) back")
         print("(c) catalog entry")
@@ -231,21 +268,42 @@ def explore_collection(entry):
             print("Unrecognized command " + cmd)
 
 
-dump_catalog = dump("_mdb_catalog")
-entries = []
-process_dump(
-    dump_catalog, handle_value=lambda entry: entries.append(decode_to_bson(entry))
-)
-dump_catalog.wait()
+def load_catalog():
+    entries = []
+
+    dump_catalog = dump("_mdb_catalog")
+    process_dump(
+        dump_catalog, handle_value=lambda entry: entries.append(decode_to_bson(entry))
+    )
+    dump_catalog.wait()
+
+    return entries
+
+
+if not wt_path:
+    wt_path = input("Path to wt: ")
+if not ksdecode_path:
+    ksdecode_path = input("Path to ksdecode (optional): ")
+if not data_path:
+    data_path = input("Path to data files: ")
+if not timestamp:
+    prompt_timestamp()
+
+entries = load_catalog()
 
 catalog_msg = "Catalog"
-header_width = len(catalog_msg)
 
 while True:
+    timestamp_msg = timestamp_str()
+    header_width = max(len(catalog_msg), len(timestamp_msg))
+
     print("*" * header_width)
     print(catalog_msg)
+    if timestamp_msg:
+        print(timestamp_msg)
     print("*" * header_width)
     print("(d) dump catalog")
+    print("(t) timestamp change")
     print("(q) quit")
 
     for i, entry in enumerate(entries):
@@ -255,6 +313,13 @@ while True:
 
     if cmd == "d":
         dump_write("_mdb_catalog", decode_value=format_to_bson)
+
+    elif cmd == "t":
+        old_timestamp = timestamp
+        prompt_timestamp()
+
+        if timestamp != old_timestamp:
+            entries = load_catalog()
 
     elif cmd == "q":
         sys.exit(0)
